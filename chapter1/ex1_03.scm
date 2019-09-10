96cd72b5e1e2c13a5104bdb42a78f818eee1e99d
jocmom <jombi@web.de>
2019-09-10 17:07:36 +0200

    ADD: Implementation of square of two largest numbers of two

--- chapter1/ex1_03.scm
+++ chapter1/ex1_03.scm
@@ -1,3 +1,15 @@
 #lang sicp
 
-(load "math.scm")
+(define (square x) (* x x))
+
+(define (sum-of-square x y)
+  (+ (square x) (square y)))
+  
+
+(define (sum-of-two-largest-squares x y z)
+  (cond ((and (> x y) (> z y))
+         (sum-of-square x z))
+        ((and (> x y) (> y z))
+         (sum-of-square x y))
+        (else (sum-of-square y z))))
+  
